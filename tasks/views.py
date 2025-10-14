4e4ac9d4c08f322d78f67d42b130878b3684f289
AdrianFiesta <122642119+AdrianFiesta@users.noreply.github.com>
2025-10-13 23:27:20 -1000

    Updated for complete button

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
+
 def dashboard(request):
     tasks = Task.objects.all()
     return render(request, "tasks/dashboard.html", {"tasks": tasks})
+
 
 def create(request):
     if request.method == "POST":
@@ -25,8 +27,9 @@ def create(request):
         return redirect("dashboard")
     return render(request, "tasks/create.html")
 
+
 def update_task(request, pk):
-    task = get_object_or_404(Task, pk = pk)
+    task = get_object_or_404(Task, pk=pk)
     if request.method == "POST":
         task.title = request.POST.get("title")
         task.deadline = request.POST.get("deadline")
@@ -39,4 +42,9 @@ def update_task(request, pk):
     return render(request, "tasks/create.html", {'task': task})
 
 
-
+def complete_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == "POST":
+        task.completed = True
+        task.save()
+    return redirect("dashboard")
